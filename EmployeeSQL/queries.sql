3479b64b058364462c4391067ed3ce70f5b483d6
AJ P <10paineaj@gmail.com>
2021-05-02 19:37:34 -0600

    Add ERD and SQL analysis 1-4

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,5 +1,4 @@
-
--- List employee number, last name, first name, and salary for each employee
+-- 1. List employee number, last name, first name, and salary for each employee
 SELECT 
 employees.emp_no, 
 employees.last_name,
@@ -7,4 +6,39 @@ employees.first_name,
 employees.sex,
 salaries.salary
 FROM employees
-INNER JOIN salaries ON salaries.emp_no = employees.emp_no; 
+INNER JOIN salaries ON salaries.emp_no = employees.emp_no;
+
+-- 2. List last name, first name and hire date for employees hired in 1986
+SELECT  
+last_name,
+first_name,
+hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+-- 3. List the manager of each department with the following information: department number,
+-- department name, the manager's employee number, last name, first name
+SELECT
+	m.dept_no,
+	d.dept_name,
+	m.emp_no,
+	e.last_name,
+	e.first_name
+FROM dept_manager AS m
+	JOIN departments as d
+	ON m.dept_no = d.dept_no
+		JOIN employees AS e
+		ON m.emp_no = e.emp_no
+
+-- 4. List the department of each employee with the following information: employee number, 
+-- last name, first name, and department name
+SELECT
+	e.emp_no,
+	e.last_name,
+	e.first_name,
+	d.dept_name
+FROM employees AS e
+	JOIN dept_emp AS de
+	ON e.emp_no = de.emp_no
+		JOIN departments AS d
+		ON de.dept_no = d.dept_no
--- EmployeeSQL/schemata.sql
+++ EmployeeSQL/schemata.sql
@@ -3,46 +3,38 @@ CREATE TABLE departments(
 	dept_name VARCHAR
 )
 
-SELECT * FROM departments
-
-DROP TABLE dept_emp
-
-CREATE TABLE dept_emp(
-	emp_no INT,
-	dept_no VARCHAR
-)
-
-SELECT * FROM dept_emp
-
-CREATE TABLE dept_manager(
-	dept_no VARCHAR,
-	emp_no INT
-)
-
-SELECT * FROM dept_manager
-
-CREATE TABLE employees(
-	emp_no INT,
-	emp_title VARCHAR,
-	birth_date	DATE,
-	first_name VARCHAR,
-	last_name VARCHAR,
-	sex VARCHAR(1),
-	hire_date DATE
-)
-
-SELECT * FROM employees
-
-CREATE TABLE salaries(
-	emp_no INT,
-	salary INT
-)
-
-SELECT * FROM salaries
-
 CREATE TABLE titles(
 	title_id VARCHAR PRIMARY KEY,
 	title VARCHAR
 )
 
-SELECT * FROM titles
+CREATE TABLE employees(
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR,
+	birth_date DATE,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	sex VARCHAR(1),
+	hire_date DATE,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+)
+
+CREATE TABLE salaries(
+	emp_no INT,
+	salary INT,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+)
+
+CREATE TABLE dept_emp(
+	emp_no INT,
+	dept_no VARCHAR,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+)
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR,
+	emp_no INT,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+)
